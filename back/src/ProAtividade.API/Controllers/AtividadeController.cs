d3910fbe83d1ea093cc5c6b4a4283dde4b73695f
Cayo Costa <caycosta@uolinc.com>
2022-11-10 22:21:33 -0300

    Add back-end

---
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Models;
+
+namespace ProAtividade.API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class AtividadeController : ControllerBase
+  {
+    public IEnumerable<Atividade> Atividades = new List<Atividade>(){
+        new Atividade("1"),
+        new Atividade("2"),
+        new Atividade("3")
+    };
+    public AtividadeController()
+    {
+
+    }
+
+    [HttpGet]
+    public Atividade GetAll()
+    {
+      return new Atividade();
+    }
+
+    [HttpGet("{id}")]
+    public Atividade GetByID(string id)
+    {
+      return Atividades.FirstOrDefault(ativ => ativ.Id == id);
+    }
+
+    [HttpPost]
+    public IEnumerable<Atividade> Post(Atividade atividade)
+    {
+      return Atividades.Append<Atividade>(atividade);
+    }
+
+    [HttpPut("{id}")]
+    public string Put(string id)
+    {
+      return "Esse é o método Edit";
+    }
+
+    [HttpDelete("{id}")]
+    public string Delete(string id)
+    {
+      return "Esse é o método Delete";
+    }
+  }
+}
